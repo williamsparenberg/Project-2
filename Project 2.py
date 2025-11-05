ee6d152d9ce60a7d4612d2d88521e0901b506575
williamsparenberg <wimsparenberg@gmail.com>
2025-11-04 22:02:28 -0500

    fixed step 1 and completed step 2 and 3
    After much testing (long process) i found the parameters that i find work best

--- Project 2.py
+++ Project 2.py
@@ -1,7 +1,10 @@
-from tensorflow import tf
+import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras import layers
-from keras.preprocessing import image_dataset_from_directory
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras import optimizers
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
 
 image_height = 500
 image_width = 500
@@ -13,39 +16,85 @@ train_dir = "/Users/williamsparenberg/Documents/GitHub/Project-2/Data/train/"
 valid_dir = "/Users/williamsparenberg/Documents/GitHub/Project-2/Data/valid/"
 
 augment_train_data = ImageDataGenerator(
-    train_dir,
-    rescale = 1./255,
-    zoom_range = .2,
-    shear_range = .2
-    )
+    rescale=1./255,   
+    zoom_range=.2,
+    shear_range=.2,
+    horizontal_flip=True
+)
 
-train_generator = image_dataset_from_directory(
-    train_dir, 
-    image_size = (image_height, image_width), 
-    label_mode = 'categorical',
-    batch_size = 32,
-    color_mode =  'rgb'
-    )
+train_generator = augment_train_data.flow_from_directory(
+    directory=train_dir,
+    target_size=(image_height, image_width), 
+    batch_size=32,
+    class_mode='categorical'
+)
 
 augment_valid_data = ImageDataGenerator(
-    valid_dir,
-    rescale = 1./255,
-    zoom_range = .2,
-    shear_range = .2
-    )
+    rescale=1./255    
+)
 
-valid_generator = image_dataset_from_directory(
-    valid_dir, 
-    image_size = (image_height, image_width), 
-    label_mode = 'categorical',
-    batch_size = 32,
-    color_mode =  'rgb'
-    )
+valid_generator = augment_valid_data.flow_from_directory(
+    directory=valid_dir,
+    target_size=(image_height, image_width),
+    batch_size=32,
+    class_mode='categorical'
+)
 
 model = Sequential([
     Conv2D(
-        filters=32,
-        kernel_size=(3, 3),
-        strides=(1, 1),
-        activation='relu',
+        filters=32, 
+        kernel_size=(3, 3), 
+        strides=(1, 1), 
+        activation='relu', 
         input_shape=input_shape
+    ),
+    MaxPooling2D(pool_size=(2, 2)), 
+    Conv2D(
+        filters=64, 
+        kernel_size=(3, 3),  
+        activation='relu'
+    ),
+    MaxPooling2D(pool_size=(2, 2)), 
+    Conv2D(
+        filters=128,             
+        kernel_size=(3, 3),  
+        activation='relu'
+    ),
+    MaxPooling2D(pool_size=(2, 2)), 
+    Flatten(), 
+    Dense(
+        units = 256, 
+        activation = 'relu'
+    ),
+    Dropout(
+        rate=0.75
+    ),
+    Dense(
+        units= 3, 
+        activation='softmax'
+    )
+])
+
+model.compile(
+    optimizer = Adam(0.0005),
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+early_stopping = EarlyStopping(
+    monitor='val_loss',     
+    patience=5,                           
+    restore_best_weights=True
+    )
+
+history = model.fit(
+    train_generator,
+    epochs=40,
+    validation_data=valid_generator,
+    callbacks=[early_stopping]
+)
+
+model.summary()
+
+model.save("best.model")
+
