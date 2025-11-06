35decbedcb6a440dc24228000d91507ff27675d2
williamsparenberg <wimsparenberg@gmail.com>
2025-11-05 22:38:25 -0500

    Just fixing syntax to make it look pretty

--- Project 2.py
+++ Project 2.py
@@ -17,8 +17,8 @@ valid_dir = "/Users/williamsparenberg/Documents/GitHub/Project-2/Data/valid/"
 #Data Prepping
 augment_train_data = ImageDataGenerator(
     rescale = 1./255,   
-    zoom_range = .2,
-    shear_range = .2,
+    zoom_range = 0.2,
+    shear_range = 0.2,
     horizontal_flip = True
 )
 
@@ -30,24 +30,24 @@ train_generator = augment_train_data.flow_from_directory(
 )
 
 augment_valid_data = ImageDataGenerator(
-    rescale=1./255    
+    rescale = 1./255    
 )
 
 valid_generator = augment_valid_data.flow_from_directory(
-    directory=valid_dir,
-    target_size=(image_height, image_width),
-    batch_size=32,
-    class_mode='categorical'
+    directory = valid_dir,
+    target_size = (image_height, image_width),
+    batch_size = 32,
+    class_mode = 'categorical'
 )
 
 #Model Creation
 model = Sequential([
     Conv2D(
-        filters=32, 
-        kernel_size=(3, 3), 
-        strides=(1, 1), 
-        activation='relu', 
-        input_shape=input_shape
+        filters = 32, 
+        kernel_size = (3, 3), 
+        strides = (1, 1), 
+        activation = 'relu', 
+        input_shape = input_shape
     ),
     MaxPooling2D(pool_size = (2, 2)), 
     Conv2D(
@@ -68,25 +68,26 @@ model = Sequential([
         activation = 'relu'
     ),
     Dropout(
-        rate=0.75
+        rate = 0.75
     ),
     Dense(
-        units= 3, 
-        activation='softmax'
+        units = 3, 
+        activation = 'softmax'
     )
 ])
 
 model.compile(
     optimizer = Adam(0.0005),
-    loss='categorical_crossentropy',
-    metrics=['accuracy']
+    loss = 'categorical_crossentropy',
+    metrics = ['accuracy']
 )
 
 early_stopping = EarlyStopping(
-    monitor='val_loss',     
-    patience=5,                           
-    restore_best_weights=True
+    monitor = 'val_loss',     
+    patience = 5,                           
+    restore_best_weights = True
     )
+
 #Hyperparameter analysis
 def plot_training_metrics(history):
     acc = history.history['accuracy']
@@ -95,19 +96,19 @@ def plot_training_metrics(history):
     val_loss = history.history['val_loss']
     epochs_range = range(len(acc))
     
-    plt.figure(figsize=(12, 5))
+    plt.figure(figsize = (12, 5))
     plt.subplot(1, 2, 1)
-    plt.plot(epochs_range, acc, label='Training Accuracy')
-    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-    plt.legend(loc='lower right')
+    plt.plot(epochs_range, acc, label = 'Training Accuracy')
+    plt.plot(epochs_range, val_acc, label = 'Validation Accuracy')
+    plt.legend(loc = 'lower right')
     plt.title('Training and Validation Accuracy')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     
     plt.subplot(1, 2, 2)
-    plt.plot(epochs_range, loss, label='Training Loss')
-    plt.plot(epochs_range, val_loss, label='Validation Loss')
-    plt.legend(loc='upper right')
+    plt.plot(epochs_range, loss, label = 'Training Loss')
+    plt.plot(epochs_range, val_loss, label = 'Validation Loss')
+    plt.legend(loc = 'upper right')
     plt.title('Training and Validation Loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
@@ -116,8 +117,8 @@ def plot_training_metrics(history):
     
 history = model.fit(
     train_generator,
-    epochs=40,
-    validation_data=valid_generator,
+    epochs = 40,
+    validation_data = valid_generator,
     callbacks=[early_stopping]
 )
 
